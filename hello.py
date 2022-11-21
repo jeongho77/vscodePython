c7469bce5e8c7be4f469d0c16e4730ba1172b123
gildong <gildong@naver.com>
2022-11-21 11:43:20 +0900

    python input 3 commit

--- hello.py
+++ hello.py
@@ -13,3 +13,8 @@ print("hello")
 a = 10
 b = 20
 print(a+b)
+
+num1 = int(input('input num1 : '))
+num2 = int(input('input num2 : '))
+# f-string (python 3.x)
+print(f"{num1}+{num2} = {num1 + num2}")
